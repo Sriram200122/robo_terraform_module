e498f77329e5307c14d770a4a789fac14ac9b996
sriram <munukutla2001@gmail.com>
2023-12-20 07:37:28 +0530

    project

--- main.tf
+++ main.tf
@@ -1,5 +1,38 @@
-resource "null_resource" "sample" {
-  provisioner "local-exec" {
-    command = "echo Hello World - Env is ${var.env}"
-  }
+################# Policy ###################
+resource "aws_iam_policy" "policy" {
+  name        = "${component}.${env}.ssm.policy"
+  path        = "/"
+  description = "Used to access the ssm parameters"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ssm:GetParameter*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
+
+resource "aws_iam_role" "test_role" {
+  name = "${component}.${env}.ec2.Role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+  ]
+
+
+
